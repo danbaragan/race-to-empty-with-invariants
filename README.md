46bb03dd973ddcaf5f0f2b7b2b01f418e00755b4
danbaragan <daniel.mihai.baragan@gmail.com>
2017-11-23 17:14:14 +0200

    Add README and explaining comments

---
+++ README.md
@@ -0,0 +1,24 @@
+# Study of race-to-empty with invariants exploit of ethereum network
+
+This is an implementation of exploiting a smart contract due to wrong invarinats + 
+too early external effects ( send() )
+This is an example of what you should not do.
+
+## Setup
+
+You need nodejs and npm.
+I have truffle and testrpc installed globaly. You need them to run this.
+Global or locally - it's your choice
+
+* ethereumjs-testrpc (@4.1.3)
+* truffle (@3.4.9)
+
+## Run
+
+Simply let `testrpc` run and do a `truffle test`
+
+## Further reading
+
+An implementation of the attack described here
+https://github.com/PeterBorah/smart-contract-security-examples/issues/3
+
--- contracts/VulnerableCoin.sol
+++ contracts/VulnerableCoin.sol
@@ -7,6 +7,8 @@ contract VulnerableCoin {
 
     modifier checkInvariants {
         _;
+        // 1
+        // If we use equality then anybody could send a small amount and make oru invariant fail (3)
         // The balance may exceed the recorded supply - this will get interesting...
         require(totalSupply <= this.balance);
     }
@@ -36,11 +38,26 @@ contract VulnerableCoin {
     {
         uint256 amount = balanceOf[msg.sender];
         if (msg.sender.call.value(amount)()) {
+            // 2
+            // External effects were executed first. Wrong.
+            // We can as well send at the end and all our changes will revert on failure (eg: revert())
+
+            // At the end of the last EvilA withdraw it will return all ether back to this contract
+            // And transfer it's amount of balaceOf tokens to EvilB
             totalSupply -= amount;
+            // This will uselessly set the balance to 0 three times in a row (see EvilA)
             balanceOf[msg.sender] = 0;
+            // At this point, after several (3 hardcoded in EvilA) withdrawals,
+            // totalSupply will be lowered, eth balance the same, and EvilB will own EvilA balance
+            // Invariant will thus pass (totalSupply > balance)
+            // and EvilB can proceed with a known rae-to-empty attack stealing up to half of
+            // VulnerableCoin balance. The remaining ether will be locked forever due to
+            // totalSupply reaching 0
         }
     }
 
+    // 3
+    // We shouldn't allow payable here, this could solve the invariant weakening in (1)
     function () payable {
 
     }
